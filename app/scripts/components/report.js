515575201f852039b5fac4c7410dcd7b6f364ad7
Derek Lieu <dl@dereklieu.com>
2019-02-18 14:26:44 -0800

    Include some basic metadata for notebook list view

--- app/scripts/components/report.js
+++ app/scripts/components/report.js
@@ -1,8 +1,14 @@
 'use strict'
 import React from 'react'
+import { ago } from 'time-ago'
+
 export default function Report ({ report }) {
+  const { created_at, forked_from } = report
+  const verb = forked_from ? 'forked' : 'created'
   return (
     <div className='report'>
+      <h3 className='report__title'>{report.name}</h3>
+      <p className='report__meta'>Author {verb} {ago(created_at)}</p>
     </div>
   )
 }
