eb1aa23956d29b7f5284d106cc5fbf7c0eadf5d0
Derek Lieu <dl@dereklieu.com>
2019-02-20 15:55:58 -0800

    Group report list by doc id

--- app/scripts/reducers/reports.js
+++ app/scripts/reducers/reports.js
@@ -1,12 +1,28 @@
 'use strict'
 import types from '../actions/types'
 import { getAsyncResponseData } from '../utils/async'
+import _groupBy from 'lodash.groupby'
 
 const initialState = []
 
 export default function reducer (state = initialState, action) {
   if (action.type === types.GET_REPORTS_SUCCESS) {
-    return getAsyncResponseData(action)
+    return aggregateReports(getAsyncResponseData(action))
   }
   return state
+}
+
+// Aggregates reports according to their `doc_id`.
+// Returns array of reports with unique `doc_id` values.
+// If input contains reports with duplicate `doc_id` values,
+// pick the one with the most recent `created_at` timestamp.
+function aggregateReports (reports) {
+  const docs = _groupBy(reports, 'doc_id')
+  for (let id in docs) {
+    docs[id] = docs[id].map(d => ({ ...d, created_at: new Date(d.created_at).getTime() }))
+      .sort((a, b) => a.created_at < b.created_at ? 1 : -1)
+  }
+  const reportList = Object.values(docs).map(docList => docList[0])
+    .sort((a, b) => a.created_at < b.created_at ? 1 : -1)
+  return reportList
 }
