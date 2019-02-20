38367ad486ba0daf1a9938893cb3766d5c0bd9df
Derek Lieu <dl@dereklieu.com>
2019-02-20 15:13:39 -0800

    Avoid camel case lint warnings

--- app/scripts/components/report.js
+++ app/scripts/components/report.js
@@ -4,12 +4,11 @@ import { Link } from 'react-router-dom'
 import { ago } from 'time-ago'
 
 export default function Report ({ report }) {
-  const { created_at, forked_from } = report
-  const verb = forked_from ? 'forked' : 'created'
+  const verb = report['forked_from'] ? 'forked' : 'created'
   return (
     <div className='report'>
       <h3 className='report__title'><Link to={`/reports/${report.id}`}>{report.name}</Link></h3>
-      <p className='report__meta'>Author {verb} {ago(created_at)}</p>
+      <p className='report__meta'>Author {verb} {ago(report['created_at'])}</p>
     </div>
   )
 }
