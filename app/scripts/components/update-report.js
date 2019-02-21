22571530138ed32405bf4b538aa1026a4caf3496
Derek Lieu <dl@dereklieu.com>
2019-02-20 16:24:36 -0800

    Hook up upload notebook actions to UI

--- app/scripts/components/update-report.js
+++ app/scripts/components/update-report.js
@@ -2,8 +2,9 @@
 import React from 'react'
 import { get } from 'object-path'
 import { connect } from 'react-redux'
+import { withRouter } from 'react-router'
 
-import { readReport } from '../actions'
+import { readReport, postReport, clearUploadState } from '../actions'
 
 import Notebook from '../components/notebook'
 
@@ -38,11 +39,48 @@ class UpdateReport extends React.Component {
       }
 
       const report = Object.assign({}, this.props.report, { content })
+      delete report['id']
+      delete report['created_at']
       this.props.readReportSuccess({ report })
     }
 
-    this.upload = (report) => {
+    this.upload = () => {
+      this.props.postReport({
+        payload: this.props.nextReport,
+        lastReport: this.props.report.id
+      })
     }
+  }
+
+  componentDidUpdate (prevProps) {
+    const { nextReportID } = this.props
+    // Report upload success: clear upload state and render the new report.
+    if (nextReportID && !prevProps.nextReportID) {
+      setTimeout(() => {
+        this.props.clearUploadState()
+        this.props.history.push(`/reports/${nextReportID}`)
+      }, 800)
+    }
+  }
+
+  renderSuccess () {
+    return (
+      <div className='success'>
+        <p>Upload successful! Loading new report...</p>
+      </div>
+    )
+  }
+
+  renderUploadNotebookUI () {
+    return (
+      <div className='report__upload'>
+        <button onClick={this.upload}>Upload this notebook</button>
+        {this.props.nextReportID && this.renderSuccess() }
+        <div className='report__upload__next'>
+          <Notebook data={this.props.nextReport} />
+        </div>
+      </div>
+    )
   }
 
   render () {
@@ -56,16 +94,17 @@ class UpdateReport extends React.Component {
             />
           </form>
         </div>
-        {this.props.nextReport && <Notebook data={this.props.nextReport} />}
+        {this.props.nextReport && this.renderUploadNotebookUI()}
       </React.Fragment>
     )
   }
 }
 
 const mapStateToProps = (state) => ({
-  nextReport: state.uploadReport.report
+  nextReport: state.uploadReport.nextReport,
+  nextReportID: state.uploadReport.nextReportID
 })
 
-const mapDispatch = { ...readReport }
+const mapDispatch = { ...readReport, postReport, clearUploadState }
 
-export default connect(mapStateToProps, mapDispatch)(UpdateReport)
+export default withRouter(connect(mapStateToProps, mapDispatch)(UpdateReport))
