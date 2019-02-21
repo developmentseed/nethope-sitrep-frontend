b426e45f37fe82ebf5d7f39e8155033236b69cdf
Derek Lieu <dl@dereklieu.com>
2019-02-20 16:00:01 -0800

    Simplify report map state

--- app/scripts/reducers/report-map.js
+++ app/scripts/reducers/report-map.js
@@ -4,10 +4,11 @@ import { getAsyncResponseData } from '../utils/async'
 
 const initialState = {}
 function reducer (state = initialState, action) {
-  if (action.type === types.GET_REPORT_SUCCESS) {
-    state = { ...state, [action.payload.id]: getAsyncResponseData(action, true) }
-  } else if (action.type === types.PATCH_REPORT_SUCCESS) {
-    state = { ...state, [action.payload.id]: getAsyncResponseData(action, true) }
+  switch (action.type) {
+    case types.GET_REPORT_SUCCESS:
+    case types.PATCH_REPORT_SUCCESS:
+    case types.POST_REPORT_SUCCESS:
+      return { ...state, [action.payload.id]: getAsyncResponseData(action, true) }
   }
   return state
 }
