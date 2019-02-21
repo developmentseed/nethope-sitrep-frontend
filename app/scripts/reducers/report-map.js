dccc6a06ea379355913902357c58af7b89d7f3a3
Derek Lieu <dl@dereklieu.com>
2019-02-21 10:55:55 -0800

    Normalize report payload to avoid errors with notebookjs

--- app/scripts/reducers/report-map.js
+++ app/scripts/reducers/report-map.js
@@ -8,8 +8,17 @@ function reducer (state = initialState, action) {
     case types.GET_REPORT_SUCCESS:
     case types.PATCH_REPORT_SUCCESS:
     case types.POST_REPORT_SUCCESS:
-      return { ...state, [action.payload.id]: getAsyncResponseData(action, true) }
+      return { ...state, [action.payload.id]: setDefaultProps(getAsyncResponseData(action, true)) }
   }
   return state
 }
 export default reducer
+
+function setDefaultProps (report) {
+  // Notebookjs will throw an exception if this is not set.
+  // https://github.com/jsvine/notebookjs/issues/21
+  if (!report.content.hasOwnProperty('metadata')) {
+    report.content.metadata = {}
+  }
+  return report
+}
