2507c2f2e60e68b8539c979f74307277e5624171
Derek Lieu <dl@dereklieu.com>
2019-02-22 18:27:10 -0800

    Standardize ID capitalization

--- app/scripts/reducers/forms.js
+++ app/scripts/reducers/forms.js
@@ -5,11 +5,11 @@ const initialState = {}
 function reducer (state = initialState, { type, payload }) {
   switch (type) {
     case types.CREATE_FORM:
-      return { ...state, [payload.formId]: payload.initialValue }
+      return { ...state, [payload.formID]: payload.initialValue }
     case types.UPDATE_FORM:
-      return { ...state, [payload.formId]: payload.value }
+      return { ...state, [payload.formID]: payload.value }
     case types.DESTROY_FORM:
-      return { ...state, [payload.formId]: null }
+      return { ...state, [payload.formID]: null }
   }
   return state
 }
