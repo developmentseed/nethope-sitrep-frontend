209b8ae14962b58d17fd79fcd9863a4eb7635a3e
Derek Lieu <dl@dereklieu.com>
2019-02-25 14:53:54 -0800

    Add settings to production config

--- app/scripts/config/production.js
+++ app/scripts/config/production.js
@@ -2,5 +2,6 @@
 
 export default {
   environment: 'production',
-  api: 'https://nethope-sitrep-api.herokuapp.com'
+  api: 'https://nethope-sitrep-api.herokuapp.com',
+  authDomain: 'nethope-sitrep.auth0.com'
 };
