17fbb0307613d91ae7f665f89c5876ac83d936d9
Derek Lieu <dl@dereklieu.com>
2019-02-25 14:08:01 -0800

    Add history fallback, include private route component

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,27 +2,54 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
-import { HashRouter, Route, Switch } from 'react-router-dom'
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
 
-import store from './store'
+import getStore from './store'
 
 import App from './components/app'
 import Home from './pages/home'
 import Reports from './pages/reports'
 import ReportDetail from './pages/report-detail'
+import Login from './pages/login'
+
+import Auth from './auth'
+const auth = new Auth()
+
+const store = getStore()
+
+class PrivateRoute extends React.Component {
+  isAuthenticated () {
+    return !!store.getState().user.token
+  }
+
+  render () {
+    const { component: Component, render: renderComponent, ...rest } = this.props
+    let render
+    if (true) {
+      render = props => renderComponent ? renderComponent(props) : <Component {...props} />
+    } else {
+      render = props => <Redirect to={{
+        pathname: '/login',
+        state: { from: props.location }
+      }} />
+    }
+    return <Route {...rest} render={render} />
+  }
+}
 
 const Root = () => (
-  <Provider store={store()}>
-    <HashRouter>
+  <Provider store={store}>
+    <BrowserRouter>
       <App>
         <Switch>
+          <Route path='/login' component={Login} />
           <Route exact path='/' component={Home} />
           <Route exact path='/reports' component={Reports} />
           <Route exact path='/reports/:reportId' component={ReportDetail} />
           <Route exact path='/reports/:reportId/update' component={ReportDetail} />
         </Switch>
       </App>
-    </HashRouter>
+    </BrowserRouter>
   </Provider>
 )
 
