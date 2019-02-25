b034061b83f34bdff8f38e3eaaa9bd72a4d6330e
Derek Lieu <dl@dereklieu.com>
2019-02-25 14:53:21 -0800

    Add nav link, transition to simple component

--- app/scripts/components/nav.js
+++ app/scripts/components/nav.js
@@ -2,17 +2,19 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 
-class Header extends React.Component {
-  render () {
-    return (
-      <nav className='nav'>
-        <ul className='nav__opts'>
-          <li className='nav__opt'><NavLink exact className='nav__link' to='/'>Home</NavLink></li>
-          <li className='nav__opt'><NavLink className='nav__link' to='/reports'>Reports</NavLink></li>
-          <li className='nav__opt'><NavLink className='nav__link' to='/emergencies'>Emergencies</NavLink></li>
-        </ul>
-      </nav>
-    )
-  }
+import Auth from './auth'
+
+const Header = () => {
+  return (
+    <nav className='nav'>
+      <ul className='nav__opts'>
+        <li className='nav__opt'><NavLink exact className='nav__link' to='/'>Home</NavLink></li>
+        <li className='nav__opt'><NavLink className='nav__link' to='/reports'>Reports</NavLink></li>
+        <li className='nav__opt'><NavLink className='nav__link' to='/emergencies'>Emergencies</NavLink></li>
+        <li className='nav__opt nav__opt--right'><Auth /></li>
+      </ul>
+    </nav>
+  )
 }
+
 export default Header
