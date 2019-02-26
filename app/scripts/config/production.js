52625f28c869108ccfdee8581113edb8a1e16463
Derek Lieu <dl@dereklieu.com>
2019-02-25 20:47:03 -0800

    Get auth0 settings from config

--- app/scripts/config/production.js
+++ app/scripts/config/production.js
@@ -4,4 +4,6 @@ export default {
   environment: 'production',
   api: 'https://nethope-sitrep-api.herokuapp.com',
   authDomain: 'nethope-sitrep.auth0.com'
+  authClientID: process.env['AUTH_CLIENT_ID'],
+  authRedirectUri: process.env['AUTH_REDIRECT_URI']
 };
