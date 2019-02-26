b4adf19ce81e1b214f1248e4f2ac0631e4b574a6
Derek Lieu <dl@dereklieu.com>
2019-02-26 15:50:06 -0800

    Create basic emergency list component

---
+++ app/scripts/components/emergency-list.js
@@ -0,0 +1,35 @@
+'use strict'
+import React from 'react'
+import { get } from 'object-path'
+import { ago } from 'time-ago'
+
+const nope = '--'
+function EmergencyList ({ data }) {
+  return (
+    <div className='em__list'>
+      <h3 className='em__title'>Emergencies</h3>
+      <table className='table'>
+        <thead className='table__head'>
+          <tr>
+            <th>Name</th>
+            <th>Type</th>
+            <th>Num. Impacted</th>
+            <th>Created</th>
+          </tr>
+        </thead>
+        <tbody className='table__body'>
+          {data.map(d => (
+            <tr key={d.id}>
+              <td>{d.name}</td>
+              <td>{get(d, 'dtype.name', nope)}</td>
+              <td>{get(d, 'num_affected')}</td>
+              <td>{ago(d['created_at'])}</td>
+            </tr>
+          )) }
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default EmergencyList
--- app/scripts/pages/country.js
+++ app/scripts/pages/country.js
@@ -1,34 +1,65 @@
 'use strict'
 import React from 'react'
 import { connect } from 'react-redux'
+import { stringify } from 'qs'
+
+import { getEmergencies } from '../actions'
 
 import AsyncStatus from '../components/async-status'
+import EmergencyList from '../components/emergency-list'
 
-function Country ({ country }) {
-  return (
-    <div className='page page__home'>
-      { country && (
-        <div className='page__header'>
+class Country extends React.Component {
+  componentDidMount () {
+    if (!this.props.emergencies) {
+      this.props.getEmergencies({ qs: this.props.qs })
+    }
+  }
+
+  componentDidUpdate (prevProps) {
+    if (!this.props.emergencies) {
+      this.props.getEmergencies({ qs: this.props.qs })
+    }
+  }
+
+  render () {
+    const { country, emergencies } = this.props
+    return (
+      <div className='page page__home'>
+        { country && (
+          <div className='page__header'>
+            <div className='inner'>
+              <h2 className='page__title'>{country.name}</h2>
+            </div>
+          </div>
+        ) }
+        <div className='section'>
           <div className='inner'>
-            <h2 className='page__title'>{country.name}</h2>
+            <AsyncStatus />
+            { emergencies && <EmergencyList data={emergencies.data} /> }
           </div>
         </div>
-      ) }
-      <div className='section'>
-        <div className='inner'>
-          <AsyncStatus />
-        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 const mapStateToProps = (state, props) => {
   const { countryID } = props.match.params
+
+  // Country meta (ie name, iso, etc).
   const country = state.countries[countryID]
+
+  // Query string to resolve this country from the GO api.
+  // Also the storage key for queried emergencies in this country.
+  const qs = stringify({ countries__in: countryID })
+  const emergencies = state.emergencies[qs]
   return {
-    country
+    country,
+    qs,
+    emergencies
   }
 }
 
-export default connect(mapStateToProps)(Country)
+const mapDispatch = { getEmergencies }
+
+export default connect(mapStateToProps, mapDispatch)(Country)
