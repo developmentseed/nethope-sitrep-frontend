d3ed64929a2569156696a11bf402d1a30f55a426
Derek Lieu <dl@dereklieu.com>
2019-02-25 20:48:44 -0800

    Fix typo

--- app/scripts/config/production.js
+++ app/scripts/config/production.js
@@ -3,7 +3,7 @@
 export default {
   environment: 'production',
   api: 'https://nethope-sitrep-api.herokuapp.com',
-  authDomain: 'nethope-sitrep.auth0.com'
+  authDomain: 'nethope-sitrep.auth0.com',
   authClientID: process.env['AUTH_CLIENT_ID'],
   authRedirectUri: process.env['AUTH_REDIRECT_URI']
 };
