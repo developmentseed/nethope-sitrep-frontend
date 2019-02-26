47cb2549e327b451003b14519764bd9b087f1cbf
Derek Lieu <dl@dereklieu.com>
2019-02-26 11:57:21 -0800

    Set GO api location

--- app/scripts/config/production.js
+++ app/scripts/config/production.js
@@ -3,6 +3,7 @@
 export default {
   environment: 'production',
   api: 'https://nethope-sitrep-api.herokuapp.com',
+  goApi: 'https://prddsgocdnapi.azureedge.net/api/v2/',
   authDomain: 'nethope-sitrep.auth0.com',
   authClientID: process.env['AUTH_CLIENT_ID'],
   authRedirectUri: process.env['AUTH_REDIRECT_URI']
