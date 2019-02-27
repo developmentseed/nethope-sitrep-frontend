fd1f557168f85bae386edf4ed4947ee82ac0c690
Derek Lieu <dl@dereklieu.com>
2019-02-26 16:48:25 -0800

    Link to GO emergency pages

--- app/scripts/components/emergency-list.js
+++ app/scripts/components/emergency-list.js
@@ -3,11 +3,15 @@ import React from 'react'
 import { get } from 'object-path'
 import { ago } from 'time-ago'
 
+function linkToGO (id) {
+  return 'https://go.ifrc.org/emergencies/' + id
+}
+
 const nope = '--'
 function EmergencyList ({ data }) {
   return (
     <div className='em__list'>
-      <h3 className='section__title'>Emergencies</h3>
+      <h3 className='section__title'>Recent Emergencies (last 30 days)</h3>
       <table className='table'>
         <thead className='table__head'>
           <tr>
@@ -20,7 +24,9 @@ function EmergencyList ({ data }) {
         <tbody className='table__body'>
           {data.map(d => (
             <tr key={d.id}>
-              <td>{d.name}</td>
+              <td>{d.name} <a href={linkToGO(d.id)} target='_blank' className='table__extlink'>
+                <span className='collecticons collecticons-expand-top-right' /></a>
+              </td>
               <td>{get(d, 'dtype.name', nope)}</td>
               <td>{get(d, 'num_affected')}</td>
               <td>{ago(d['created_at'])}</td>
