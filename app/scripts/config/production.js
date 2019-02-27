051ff43a5e708e0c61dfcf9df0efe2fa5a383e9a
Derek Lieu <dl@dereklieu.com>
2019-02-26 16:22:22 -0800

    Check in access token

--- app/scripts/config/production.js
+++ app/scripts/config/production.js
@@ -2,8 +2,12 @@
 
 export default {
   environment: 'production',
+
+  mbtoken: 'pk.eyJ1IjoiZ28taWZyYyIsImEiOiJjams3b2ZhZWswMGFvM3hxeHp2ZHFhOTRrIn0._pqO9OQ2iNeDGrpopJNjpg',
+
   api: 'https://nethope-sitrep-api.herokuapp.com',
   goApi: 'https://prddsgocdnapi.azureedge.net/api/v2/',
+
   authDomain: 'nethope-sitrep.auth0.com',
   authClientID: process.env['AUTH_CLIENT_ID'],
   authRedirectUri: process.env['AUTH_REDIRECT_URI']
