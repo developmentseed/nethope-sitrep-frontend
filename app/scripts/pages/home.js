b0a2344729b4aef29c6a6010c1ea57ea98bb37e8
Derek Lieu <dl@dereklieu.com>
2019-03-14 11:46:34 -0700

    Render featured emergencies on homepage

--- app/scripts/pages/home.js
+++ app/scripts/pages/home.js
@@ -1,8 +1,27 @@
 'use strict'
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { stringify } from 'qs'
+
+import { getStaticApi, getEmergencies } from '../actions'
+
+import EmergencyList from '../components/emergency-list'
+import AsyncStatus from '../components/async-status'
+
 class Home extends React.Component {
+  componentDidMount () {
+    this.props.getStaticApi({ resource: 'featured-emergencies.json' })
+  }
+
+  componentDidUpdate () {
+    if (!this.props.emergencies) {
+      this.props.getEmergencies({ qs: this.props.qs })
+    }
+  }
+
   render () {
+    const { emergencies } = this.props
     return (
       <div className='page page__home'>
         <div className='page__header'>
@@ -14,10 +33,27 @@ class Home extends React.Component {
           <div className='inner'>
             <h3 className='section__title'><Link to='/reports'>Reports</Link></h3>
             <h3 className='section__title'><Link to='/emergencies'>Emergencies</Link></h3>
+            <AsyncStatus />
+            { emergencies && <EmergencyList data={emergencies.data} title='Active Emergencies' showCountry={true} /> }
           </div>
         </div>
       </div>
     )
   }
 }
-export default Home
+
+const mapStateToProps = (state) => {
+  const { featured } = state
+  const qs = stringify({
+    id__in: featured.join(',')
+  })
+
+  const emergencies = state.emergencies[qs]
+  return {
+    featured,
+    qs,
+    emergencies
+  }
+}
+const mapDispatch = { getStaticApi, getEmergencies }
+export default connect(mapStateToProps, mapDispatch)(Home)
