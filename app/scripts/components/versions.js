8c58a84ffabb831605074e1b3c544c21fffd3a14
Derek Lieu <dl@dereklieu.com>
2019-03-18 18:01:16 -0700

    Show version links as an option

--- app/scripts/components/versions.js
+++ app/scripts/components/versions.js
@@ -1,11 +1,23 @@
 'use strict'
 import React from 'react'
+import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { get } from 'object-path'
+import { ago } from 'time-ago'
 
 import { getReportVersions } from '../actions'
 
 class Versions extends React.Component {
+  constructor (props) {
+    super(props)
+    this.toggleOld = this.expand.bind(this, 'older')
+    this.toggleNew = this.expand.bind(this, 'newer')
+    this.state = {
+      older: false,
+      newer: false
+    }
+  }
+
   componentDidMount () {
     this.getVersions()
   }
@@ -14,18 +26,44 @@ class Versions extends React.Component {
     if (this.props.docID !== prevProps.docID) {
       this.getVersions()
     }
+
+    if (this.props.current !== prevProps.current) {
+      this.setState({
+        older: false,
+        newer: false
+      })
+    }
   }
 
   getVersions () {
     this.props.getReportVersions({ docID: this.props.docID })
   }
 
+  expand (propName, e) {
+    e.preventDefault()
+    this.setState({ [propName]: !this.state[propName] })
+  }
+
+  renderVersionLinks (versions) {
+    return (
+      <ul className='versions'>
+        {versions.map(d => <li key={d.id}>
+          <span className='collecticons collecticons-pages'/>&nbsp;
+          <Link to={`/reports/${d.id}`}>{d.name}</Link>&nbsp;
+          <span className='light'>created {ago(d.created_at)}</span>
+        </li>)}
+      </ul>
+    )
+  }
+
   render () {
     const { older, newer } = this.props
     return (
       <div className='report__versions'>
-        { older.length > 0 && <p className='versions__count'>{older.length} older version(s) available.</p> }
-        { newer.length > 0 && <p className='versions__count'>{newer.length} newer version(s) available.</p> }
+        { older.length > 0 && <p className='versions__count'>{older.length} older version(s) available. <a href='#' onClick={this.toggleOld}>{this.state.older ? 'Hide' : 'Show'}</a></p> }
+        { this.state.older && this.renderVersionLinks(older) }
+        { newer.length > 0 && <p className='versions__count'>{newer.length} newer version(s) available. <a href='#' onClick={this.toggleNew}>{this.state.newer ? 'Hide' : 'Show'}</a></p> }
+        { this.state.newer && this.renderVersionLinks(newer) }
       </div>
     )
   }
@@ -44,8 +82,9 @@ const mapStateToProps = (state, props) => {
   const versions = Object.values(reportMap)
     .filter(report => report['doc_id'] === docID)
   return {
-    older: versions.filter(d => d['created_at'] < createdAt).map(d => d.id),
-    newer: versions.filter(d => d['created_at'] > createdAt).map(d => d.id)
+    older: versions.filter(d => d['created_at'] < createdAt).sort((a, b) => a['created_at'] > b['created_at'] ? -1 : 1),
+    newer: versions.filter(d => d['created_at'] > createdAt).sort((a, b) => a['created_at'] > b['created_at'] ? -1 : 1),
+    current
   }
 }
 
