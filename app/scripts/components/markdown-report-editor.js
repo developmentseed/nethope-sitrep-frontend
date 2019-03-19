c900a716d4bbb91b0a36f1776fa1ce688edaba14
Derek Lieu <dl@dereklieu.com>
2019-03-19 14:24:10 -0700

    Make the markdown component a pure component

--- app/scripts/components/markdown-report-editor.js
+++ app/scripts/components/markdown-report-editor.js
@@ -6,26 +6,18 @@ import { Value } from 'slate'
 import _initialValue from '../utils/initial-editor-value.json'
 const initialValue = Value.fromJSON(_initialValue)
 
-class MarkdownReportEditor extends React.Component {
-  constructor (props) {
-    super(props)
-    this.onChange = (change) => {
-      console.log(change)
-    }
-  }
-
-  render () {
-    return (
-      <div className='editor__cont'>
-        <Editor
-          id='new-report-slate-editor'
-          defaultValue={initialValue}
-          onChange={this.onChange}
-          className='editor'
-        />
-      </div>
-    )
-  }
+function MarkdownReportEditor ({ onChange, value }) {
+  return (
+    <div className='editor__cont'>
+      <Editor
+        id='new-report-slate-editor'
+        defaultValue={initialValue}
+        onChange={onChange}
+        value={value}
+        className='editor'
+      />
+    </div>
+  )
 }
 
 export default MarkdownReportEditor
