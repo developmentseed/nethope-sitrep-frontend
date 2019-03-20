374752a045d257352f872528f65db6200ef220d1
Derek Lieu <dl@dereklieu.com>
2019-03-20 09:37:47 -0700

    Render emergency select for new reports

--- app/scripts/pages/new-report.js
+++ app/scripts/pages/new-report.js
@@ -4,13 +4,16 @@ import { connect } from 'react-redux'
 import Plain from 'slate-plain-serializer'
 
 import { getSimpleNotebookPayload } from '../utils/async'
-import { createReport, postReport, forms } from '../actions'
+import { recentQs } from '../utils/timespans'
+import { createReport, postReport, forms, getEmergencies } from '../actions'
 
 import AsyncStatus from '../components/async-status'
 import MarkdownReportEditor from '../components/markdown-report-editor'
 import EditableText from '../components/editable-text'
 import UploadReportSuccess from '../components/upload-report-success'
 import ReactSelect from '../components/react-select'
+import Select from '../components/select'
+import EmergencyList from '../components/emergency-list'
 
 import _disasterTypes from '../../static/disaster-types.json'
 import _reportTypes from '../../static/report-types.json'
@@ -21,7 +24,7 @@ const reportTypes = _reportTypes.map(d => ({ label: d.type, value: d.type }))
 const themes = _themes.map(d => ({ label: d.theme, value: d.theme }))
 
 const nameFieldID = 'new-report-name'
-const countryFormID = 'new-report-country'
+const emergencyFieldID = 'new-report-emergency'
 
 class NewReport extends React.Component {
   constructor (props) {
@@ -46,13 +49,34 @@ class NewReport extends React.Component {
       this.setState(nextState)
     }
 
-    this.selectCountry = (e) => {
+    this.selectEmergency = (e) => {
       e.preventDefault()
+      const value = e.currentTarget.getAttribute('data-name')
       this.props.update({
-        formID: countryFormID,
-        value: e.currentTarget.getAttribute('data-value')
+        formID: emergencyFieldID,
+        value
       })
     }
+  }
+
+  componentDidMount () {
+    // We need to query emergencies here to support metadata selection
+    if (!this.props.emergencies) {
+      this.props.getEmergencies({ qs: this.props.qs })
+    }
+  }
+
+  renderEmergencyTable () {
+    const { emergencies } = this.props
+    if (!emergencies || !emergencies.data.length) return <AsyncStatus />
+    return (
+      <div className='modal__select modal__select--emergency'>
+        <EmergencyList
+          isSelect={true}
+          onRowSelect={this.selectEmergency}
+          data={emergencies.data} />
+      </div>
+    )
   }
 
   render () {
@@ -91,6 +115,12 @@ class NewReport extends React.Component {
               value={this.props.editorValue}/>
 
             <div className='tags'>
+              <Select formID={emergencyFieldID}
+                label='Emergency'
+                placeholder='Select an emergency...'
+                prompt='Choose an emergency'>
+                {this.renderEmergencyTable()}
+              </Select>
               <ReactSelect label='Country' options={countries} />
             </div>
 
@@ -113,7 +143,14 @@ class NewReport extends React.Component {
 const mapStateToProps = (state) => ({
   editorValue: state.newReport.value,
   name: state.forms[nameFieldID],
-  countries: state.countries || {}
+  countries: state.countries || {},
+  emergencies: state.emergencies[recentQs],
+  qs: recentQs
 })
-const mapDispatch = { ...createReport, postReport, update: forms.update }
+const mapDispatch = {
+  ...createReport,
+  postReport,
+  getEmergencies,
+  update: forms.update
+}
 export default connect(mapStateToProps, mapDispatch)(NewReport)
