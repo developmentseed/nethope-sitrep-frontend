0a9d942825751c643ff3712c386bcb053b9c2d8a
Derek Lieu <dl@dereklieu.com>
2019-03-20 14:12:26 -0700

    Use a counter for better load tracking

--- app/scripts/reducers/async.js
+++ app/scripts/reducers/async.js
@@ -3,7 +3,7 @@ import types from '../actions/types'
 
 const initialState = {
   type: null,
-  loading: false,
+  loading: 0,
   error: null
 }
 
@@ -15,19 +15,18 @@ const asyncTypes = [
   types.READ_REPORT,
   types.POST_REPORT,
   types.GET_COUNTRIES,
-  types.GET_EMERGENCIES,
-  types.GET_STATIC_API
+  types.GET_EMERGENCIES
 ]
 const SUCCESS = asyncTypes.map(s => s + '_SUCCESS')
 const FAIL = asyncTypes.map(s => s + '_FAIL')
 
 export default function reducer (state = initialState, { type, payload, error }) {
   if (asyncTypes.indexOf(type) >= 0) {
-    return { ...state, type, loading: true }
+    return { ...state, type, loading: state.loading + 1 }
   } else if (SUCCESS.indexOf(type) >= 0) {
-    return { ...state, type, loading: false, error: null }
+    return { ...state, type, loading: state.loading - 1, error: null }
   } else if (FAIL.indexOf(type) >= 0) {
-    return { ...state, type, loading: false, error: error }
+    return { ...state, type, loading: state.loading - 1, error: error }
   }
   return state
 }
